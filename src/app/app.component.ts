6d0bb20902efa21dc51c9f1e4ee07d2d03b66341
Sameer <dholesameer@yahoo.in>
2020-08-25 13:25:47 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, Inject } from '@angular/core';
+import {MatDialogRef, MatDialog} from '@angular/material';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'confusion';
+}
